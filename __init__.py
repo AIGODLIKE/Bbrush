bf540d43b7e624411175840ff8f16e360cabe924
小萌新 <3209970865@qq.com>
2025-05-12 10:57:40 +0800

    Feat: update version 1.2.8 -> 1.3.0

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from . import register_module
 bl_info = {
     "name": "Bbrush",
     "author": "AIGODLIKE Community：小萌新",
-    "version": (1, 2, 8),
+    "version": (1, 3, 0),
     "blender": (4, 0, 0),
     "location": "Entering the sculpt mode will be displayed in the top bar",
     "description": "",
