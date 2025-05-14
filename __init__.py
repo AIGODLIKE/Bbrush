0a3cf4774f1d1d8c27cec617d009a32c2de33451
小萌新 <3209970865@qq.com>
2025-05-14 15:02:55 +0800

    Feat: update version 1.3.0 -> 1.3.1

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from . import register_module
 bl_info = {
     "name": "Bbrush",
     "author": "AIGODLIKE Community：小萌新",
-    "version": (1, 3, 0),
+    "version": (1, 3, 1),
     "blender": (4, 0, 0),
     "location": "Sculpt mode",
     "description": "Sculpting using only a pen and keyboard",
